cf07e2a0a4f58ef2ddad920e8b2416e4a2d1b7e8
ZYTRUST SA <100867119+ZYTRUST@users.noreply.github.com>
2025-03-21 16:40:21 -0500

    Cambios en ls fuentes

---
+++ app/src/main/java/com/zy/zyuifalabella/activity/TestActivity.kt
@@ -0,0 +1,16 @@
+package com.zy.zyuifalabella.activity
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.zy.zyuifalabella.R
+
+class TestActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test)
+        //var test = ZyProgressDialog(this)
+    }
+}
