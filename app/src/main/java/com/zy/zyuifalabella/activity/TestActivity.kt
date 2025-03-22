084e7157eeb18f0aefe5112dc4f30ff97fa2d22b
ZYTRUST SA <100867119+ZYTRUST@users.noreply.github.com>
2025-03-21 19:37:09 -0500

    Cambios en ls fuentes

--- app/src/main/java/com/zy/zyuifalabella/activity/TestActivity.kt
+++ app/src/main/java/com/zy/zyuifalabella/activity/TestActivity.kt
@@ -2,12 +2,13 @@ package com.zy.zyuifalabella.activity
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.zy.zylibuitestandroid.ui.dialogs.ZyProgressDialog
 import com.zy.zyuifalabella.R
 
 class TestActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_test)
-        //var test = ZyProgressDialog(this)
+        //var test = ZyProgressDialog()
     }
 }
